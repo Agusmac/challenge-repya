6be8849e966e4734ee0d84b706945db89714b797
Agusmac <agusmac40@gmail.com>
2022-05-30 17:59:16 -0300

    hmmm

---
+++ dummy.js
@@ -0,0 +1,30 @@
+export const dummyDatax = [
+    {
+      clientId: 1,
+      document: 42728328,
+      email: 'agus@gmail.com',
+      action: 'compra',
+      observations: 'le fish au chocolat',
+      date: '18/09/2022'
+    },
+    {
+      clientId: 2,
+      document: 12728328,
+      email: 'jesus@gmail.com',
+      action: 'consulta',
+      observations: 'jadsadasdsdkjasnbdkajsn',
+      date: '18/09/2022'
+    },
+    {
+      clientId: 3,
+      document: 36728328,
+      email: 'messi@gmail.com',
+      action: 'llamado',
+      observations: 'lorem ipsum generico',
+      date: '27/10/2022'
+    },
+  
+  ]
+
+  
+  export const dummyIDs = [1, 2, 3, 64, 128, 256]
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,40 @@
 import React, { useState, useRef } from 'react'
 // import styles from '../styles/Home.module.css'
 import List from '../components/List'
+import { dummyIDs,dummyDatax } from '../dummy'
+// const dummyIDs = [1, 2, 3, 64, 128, 256]
 
-const dummyIDs = [1, 2, 3, 64, 128, 256]
+// const dummyDatax = [
+//   {
+//     clientId: 1,
+//     document: 42728328,
+//     email: 'agus@gmail.com',
+//     action: 'compra',
+//     observations: 'le fish au chocolat',
+//     date: '18/09/2022'
+//   },
+//   {
+//     clientId: 2,
+//     document: 12728328,
+//     email: 'jesus@gmail.com',
+//     action: 'consulta',
+//     observations: 'jadsadasdsdkjasnbdkajsn',
+//     date: '18/09/2022'
+//   },
+//   {
+//     clientId: 3,
+//     document: 36728328,
+//     email: 'messi@gmail.com',
+//     action: 'llamado',
+//     observations: 'lorem ipsum generico',
+//     date: '27/10/2022'
+//   },
 
-const dummyDatax = [
-  {
-    clientId: 1,
-    document: 42728328,
-    email: 'agus@gmail.com',
-    action: 'compra',
-    observations: 'le fish au chocolat',
-    date: '18/09/2022'
-  },
-  {
-    clientId: 2,
-    document: 12728328,
-    email: 'jesus@gmail.com',
-    action: 'consulta',
-    observations: 'jadsadasdsdkjasnbdkajsn',
-    date: '18/09/2022'
-  },
-  {
-    clientId: 3,
-    document: 36728328,
-    email: 'messi@gmail.com',
-    action: 'llamado',
-    observations: 'lorem ipsum generico',
-    date: '27/10/2022'
-  },
-
-]
+// ]
 
 
 export default function Home() {
 
-  // compra,llamado,consulta
 
   const [open, setOpen] = useState(false)
   const [currentUser, setCurrentUser] = useState('')
